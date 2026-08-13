d9fa6031c5a830367b50657a7928d078617e9e6b
Mohammed <ourimpactldn@gmail.com>
2026-08-13 11:47:46 +0100

    Align AIReadinessAssessment width with membership section (900px max-width)

--- src/App.jsx
+++ src/App.jsx
@@ -4014,7 +4014,7 @@ useEffect(() => {
           boxSizing: "border-box",
         }}
       >
-        <div style={{ maxWidth: "1280px", margin: "0 auto" }}>
+        <div style={{ maxWidth: "900px", margin: "0 auto" }}>
           <AIReadinessAssessment onConsult={() => onNavigate("services")} />
         </div>
       </section>
